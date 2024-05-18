836ee17967406064ba5d242e5c9f1d7b15519659
Ravi Kumar <105579910+ravisingh231218@users.noreply.github.com>
2024-05-18 19:25:48 +0530

    Update ApplicationController.cs

--- Ado.NetCrudWith_Procedure/Controllers/ApplicationController.cs
+++ Ado.NetCrudWith_Procedure/Controllers/ApplicationController.cs
@@ -59,3 +59,71 @@ namespace Ado.NetCrudWith_Procedure.Controllers
         // You can add other action methods as needed
     }
 }
+using Ado.NetCrudWith_Procedure.DAL;
+using Ado.NetCrudWith_Procedure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Ado.NetCrudWith_Procedure.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        private readonly Employee_DL _employeeDL;
+
+        public ApplicationController(IConfiguration configuration)
+        {
+            _employeeDL = new Employee_DL(configuration);
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeDL.GetAllEmployees();
+            return View(employees);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeDL.InsertEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        public IActionResult Update(int id)
+        {
+            var employee = _employeeDL.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Update(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeDL.UpdateEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeDL.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _employeeDL.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+        // You can add other action methods as needed
+    }
+}
